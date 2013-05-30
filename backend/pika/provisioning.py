08db73f4447da65ed97164154db3a08980f0c752
Luka Perkov <freeacs-ng@lukaperkov.net>
2013-05-30 13:18:21 +0200

    add missing copyright in backend pika scripts
    Signed-off-by: Luka Perkov <freeacs-ng@lukaperkov.net>

--- backend/pika/provisioning.py
+++ backend/pika/provisioning.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python2
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Copyright (C) 2013 Luka Perkov <freeacs-ng@lukaperkov.net>
+#
 
 import base64
 import json
